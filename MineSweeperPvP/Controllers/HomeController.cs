1f60adc06fab445d0a818f21c9565f9eda8a9f29
cabinarronn <143042488+PieckFingerr@users.noreply.github.com>
2025-11-18 09:34:43 +0700

    V2

--- MineSweeperPvP/Controllers/HomeController.cs
+++ MineSweeperPvP/Controllers/HomeController.cs
@@ -1,12 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-
 namespace MineSweeperPvP.Controllers
 {
     public class HomeController : Controller
     {
         public IActionResult Index()
         {
+            var userId = HttpContext.Session.GetInt32("UserId");
+            var username = HttpContext.Session.GetString("Username");
+
+            ViewBag.IsLoggedIn = userId.HasValue;
+            ViewBag.Username = username;
+
+            return View();
+        }
+
+        public IActionResult Main()
+        {
+            var userId = HttpContext.Session.GetInt32("UserId");
+            if (userId == null)
+                return RedirectToAction("Login", "Account");
+
+            ViewBag.Username = HttpContext.Session.GetString("Username");
             return View();
         }
     }
--- MineSweeperPvP/Program.cs
+++ MineSweeperPvP/Program.cs
@@ -3,18 +3,37 @@ using MineSweeperPvP.Datas;
 using MineSweeperPvP.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services
 builder.Services.AddControllersWithViews();
 builder.Services.AddSignalR();
+
+// Add Session support
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+
 app.UseStaticFiles();
 app.UseRouting();
+
+// Enable session
+app.UseSession();
+
 app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapHub<GameHub>("/gamehub");
+
 app.Run();
